f2cca41f48dc40ea2467135c769a8cf33a3b3939
plebchan <ahelewhaatia@gmail.com>
2019-02-23 04:42:35 -0800

    changed ip

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 import socket
 from time import sleep
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('192.168.1.112',32792)
+ip =socket.gethostbyname(socket.gethostname())
+server_address = ("",8653)
 client.connect(server_address)
+print(client)
 message = 'Hi from the client'
 message = message.encode('utf-8')
 client.send(message)
 data = client.recv(1024).decode('utf-8')
 print (data)
-sleep(10)
 client.close()
