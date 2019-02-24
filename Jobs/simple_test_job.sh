17810be328fafa2550112af89b3ebff4673b8a83
plebchan <ahelewhaatia@gmail.com>
2019-02-24 05:45:39 +0200

    adding job script

---
+++ Jobs/simple_test_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#SBaATCH --job-name=Tunneling_test
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+
+source ~/data/anaconda3/bin/activate
+
+
+cd ~/data
+
+echo "Start Testing ..................................."
+
+python -u test.py
